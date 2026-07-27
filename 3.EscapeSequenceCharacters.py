b29c470c3a869adf41ee2c4d44e1e7e0537cc5b0
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-27 22:35:37 +0530

    3.EscapeSequenceCharacters.py

---
+++ 3.EscapeSequenceCharacters.py
@@ -0,0 +1,6 @@
+print("I am a good girl\nand they are also good boy/girl")
+
+# by using \n we can print the next sentence in a new line
+print("I am a \"good girl\"\nand they are also good boy/girl")
+
+# an escape sequence character is a backslash character \ followed by the character you want to insert
