e913666d9e8471797640248109d2b92bd354233b
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-27 22:31:49 +0530

    1.print.py

---
+++ 1.print.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print("Aishwarya")
+print(5)
+print(0.8)
+print(8*9)
+print("hey", 6, 7, sep="~", end="009\n") # sep 'Separator' - specifies how to separate an object
+print("Aishwarya") # end - specifies what to print at the end
