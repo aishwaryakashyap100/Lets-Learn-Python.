bc0c3afd46c94552569d4f10b076a256b89f7b3a
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-28 12:35:39 +0530

    7.calculator.py

--- 7.calculator.py
+++ 7.calculator.py
@@ -1,18 +1,7 @@
-# Simple calculator program
+a = 6
+b = 7
 
-print("Enter the first number :")
-a = int(input())
-print("Enter the second number :")
-b = int(input())
-
-print("Enter the operation you want to perform : + _ * /")
-operation = input()
-
-if operation =='+':
-    print("The addition of two numbers is :",a+b)
-if operation =='_':
-    print("The subtraction of two numbers is :",a-b)
-if operation =='*':
-    print("The multiplication of two numbers is :",a*b)
-if operation =='/':
-    print("The division of two numbers is :",a/b)
+print("The addition of two numbers is :", a+b)
+print("The subtraction of two numbers is :", a-b)
+print("The multiplication of two numbers is :", a*b)
+print("The division of two numbers is :", a+b)
