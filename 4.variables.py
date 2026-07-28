ab5199a72da66e6419dc1e3c08f782f47b8d9772
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-28 11:47:29 +0530

    4.variables.py

---
+++ 4.variables.py
@@ -0,0 +1,15 @@
+# Variables is like container that holds data
+
+a = "Aditi"
+b = "Aishwarya"
+c = "Anuradha"
+a1 = 20 # age of aditi
+b2 = 18 # age of aishwarya
+c3 = 15 # age of anuradha
+
+print("Three sisters live together with their parents and their names are", a, b, c, "and their ages are", a1, b2, c3)
+
+# We can also know the type of the data by typecasting
+
+print("The type of a is",type(a))
+print("The type of b2 is",type(b2))
