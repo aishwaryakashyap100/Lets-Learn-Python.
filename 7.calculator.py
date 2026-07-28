905e24194f0d22541a83f9114e456f036c50b540
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-28 12:40:06 +0530

    7.ProgramCalculator.py

--- 7.calculator.py
+++
@@ -1,7 +0,0 @@
-a = 6
-b = 7
-
-print("The addition of two numbers is :", a+b)
-print("The subtraction of two numbers is :", a-b)
-print("The multiplication of two numbers is :", a*b)
-print("The division of two numbers is :", a+b)
