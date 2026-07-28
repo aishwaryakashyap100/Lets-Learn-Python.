ef606119188fd8477054bdf383c0edb22d201642
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-28 12:18:16 +0530

    6.operators.py

---
+++ 6.operators.py
@@ -0,0 +1,22 @@
+# Python have different types of operators
+
+addition = 15+7
+print(addition)
+
+subtraction = 15-7
+print(subtraction)
+
+multiplication = 15*7
+print(multiplication)
+
+division = 15/7
+print(division)
+
+modulus = 15%7 
+print(modulus)
+
+floordivision = 15//7
+print(floordivision)
+
+exponential = 15**7
+print(exponential)
