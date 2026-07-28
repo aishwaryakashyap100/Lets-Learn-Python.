a29a14261b386d7693b0238b63bb203fd52e78da
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-28 12:42:06 +0530

    7.ProgramCalculator.py

---
+++ 7.ProgramCalculator.py
@@ -0,0 +1,7 @@
+a = 6
+b = 7
+
+print("The addition of two numbers is :", a+b)
+print("The subtraction of two numbers is :", a-b)
+print("The multiplication of two numbers is :", a*b)
+print("The division of two numbers is :", a+b)
