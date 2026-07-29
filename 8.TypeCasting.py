5330afc4c621195f5e0c816e624f084ffe440a67
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-29 20:10:52 +0530

    8.TypeCasting.py

---
+++ 8.TypeCasting.py
@@ -0,0 +1,17 @@
+# Typecasting is the conversion of one data type into the other data type
+
+a = "5"
+b = "9"
+print(int(a)+int(b)) # If we are converting any data type then it should be valid
+
+# Typecasting is of two types explicit and implicit
+
+# Implicit - done by the python
+one = 3.9
+two = 7
+print(one+two)
+
+# Explicit - done by the programmer
+c = '4'
+d = 3
+print("The sum of both the numbers is :",int(c)+d)
