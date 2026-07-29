eab4db680c5088357d0a7cd7223bdcd46259f0fb
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-29 20:33:06 +0530

    9.input.py

---
+++ 9.input.py
@@ -0,0 +1,10 @@
+a = input()
+print("Hello!",a)
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+print(x+y)
+
+# If you want a valid output typecasting can be done for this
+# If you want the answer in interger then it must be typecasted otherwise python concatanates it
+# Meaning of concatanates - it adds the first output with the second
