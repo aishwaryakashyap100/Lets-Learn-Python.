4c16bf4bc6edf0c8c5caa0f91caa2c97437b70ad
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-07-30 13:34:58 +0530

    String Methods

---
+++ 11.StringMethods.py
@@ -0,0 +1,10 @@
+names = "Aishwarya,Siddhi"
+print(len(names)) # Finds the length of the string
+
+#strings as an array -
+print(names[0:5]) #It prints the first five characters
+# This is also known as slicing
+print(names[0:-5]) # It minus the ending characters and print the staring ones whcih is left
+print(names[-3:-1])
+a = 'Aishwarya'
+print(a[-5:-3])
