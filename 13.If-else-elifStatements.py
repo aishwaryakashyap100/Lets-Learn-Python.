35372a34000fe171c95c4d197f8ab4ace497dcca
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-08-01 21:52:51 +0530

    If-else-elif Statements

---
+++ 13.If-else-elifStatements.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the num: "))
+
+if num<0:
+    print("Number is negative")
+elif num==0:
+    print("Number is positive")
+elif num==999:
+    print("Number is special")
+else:
+    print("Number is positive")
+
+print("Done")
