3c54ca11dc1df1720c6aecdc150ce4fe3cdafb8a
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-08-04 20:41:14 +0530

    Break Statement

---
+++ 19-BreakStatements.py
@@ -0,0 +1,10 @@
+# Break - The break statement enables the program to skip over a part of code
+
+for i in range(12):
+    print("5 X", i+1, "=", 5*(i+1))
+    if i==10:
+        break
+
+print("Get out of the loop/Skip the loop")
+# This break program prints the table of 5
+
