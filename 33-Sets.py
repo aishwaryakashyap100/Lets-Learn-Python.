79212ebb56440c4d6aaa25c346514939d509949b
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-08-13 21:55:49 +0530

    Sets

---
+++ 33-Sets.py
@@ -0,0 +1,17 @@
+# Sets are ordered collection of data items
+
+s = {3,5,2,4,5,3}
+print(s)
+
+info = {'Carla',19,False,5.9,19}
+print(info)
+
+s = {}
+print(type(s))
+
+s = set()
+print(type(s))
+
+info = {'Carla',19,False,5.9,19}
+for item in info:
+    print(item)
