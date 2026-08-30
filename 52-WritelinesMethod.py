07470da4393e9a9ef3f7d5eb44e746cc8042e3aa
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-08-30 22:26:44 +0530

    Writelines Method

---
+++ 52-WritelinesMethod.py
@@ -0,0 +1,10 @@
+# f = open('myfile4.txt','w')
+# lines = ['line 1\n', 'line 2\n', 'line 3\n']
+# f.writelines(lines)
+# f.close
+
+f = open('myfile4.txt','w')
+lines2 = ['line 4', 'line 5', 'line 6']
+for line in lines2:
+    f.write(line +'\n')
+f.close()
