647a34856bb1c22c9aa0c23c93c3e61e88055617
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-09-01 21:58:16 +0530

    Lambda Functions

---
+++ 54-LambdaFunctions.py
@@ -0,0 +1,15 @@
+# def double(x):
+#     return x*2
+
+double = lambda x:x*2
+cube = lambda x:x*x*x
+avg = lambda x,y:(x+y)/2
+
+print(double(5))
+print(cube(5))
+print(avg(4,7))
+
+def appl(fx,value):
+    return 6+fx(value)
+
+print(appl(cube,2))
