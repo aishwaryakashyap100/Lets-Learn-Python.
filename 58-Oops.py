02c8fe6278b3c50a9ae2c5329b9702b3fb968d5c
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-09-07 22:31:06 +0530

    OOPS

---
+++ 58-Oops.py
@@ -0,0 +1,19 @@
+def hello():
+    print('Hello')
+
+hello()
+
+sales1 = 6000
+profit1 = 2000
+ad3 = 1000
+# Manwa.sales
+
+sales1 = 6000
+profit1 = 2000
+ad3 = 1000
+# Kanwa.sales
+
+sales1 = 6000
+profit1 = 2000
+ad3 = 1000
+# Pitu.sales
