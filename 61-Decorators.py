f204930eae8a78f14a0692020503d4a9fffa5225
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2026-09-10 22:43:27 +0530

    Decorators

---
+++ 61-Decorators.py
@@ -0,0 +1,34 @@
+def greet(fx):
+    def mfx(*args,**kwargs):
+        print('Good Morning')
+        fx(*args,**kwargs)
+        print('Thanks for using this function')
+    return mfx
+
+@greet
+def hello():
+    print('Hello world')
+
+def add(a,b):
+    print(a+b)
+
+# greet(hello)()
+hello()
+# greet(add)(1,2)
+greet(add)(1,2)
+
+# Practical use case -
+
+import logging
+
+def log_function_call(func):
+    def decorated(*args,**kwargs):
+        logging.info(f'Calling{func.__name__} with args = {args},kwargs = {kwargs}')
+        result = func(*args,**kwargs)
+        logging.info(f'{func.__name__}returned{result}')
+        return result
+    return decorated
+
+@log_function_call
+def my_function(a,b):
+    return a+b
